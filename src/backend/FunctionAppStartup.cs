5ec1c9481486bfdc1e4b1ad3eca4e601ce7cabf0
Rafael Turino <rafael.turino@arctouch.com>
2019-04-02 15:50:03 -0300

    Add the functions project

---
+++ src/backend/FunctionAppStartup.cs
@@ -0,0 +1,18 @@
+﻿using FunctionMonkey.Abstractions;
+using FunctionMonkey.Abstractions.Builders;
+
+namespace ServerlessTodo
+{
+    public class FunctionAppStartup : IFunctionAppConfiguration
+    {
+        public void Build(IFunctionHostBuilder builder)
+        {
+            builder
+                .Setup((serviceCollection, commandRegistry) =>
+                {
+                })
+                .Functions(functions =>
+                );
+        }
+    }
+}
